eee5d6d589e9498337137e7e9cd46fa5b2db15d8
Leigh Murray <leighmurray@2wattend.com>
2016-03-12 16:04:32 +1000

    added service provider

---
+++ ServiceProviders/PaypalServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CMSilex\Paypal\ServiceProviders;
+
+use Silex\Application;
+use Silex\ServiceProviderInterface;
+
+class PayPalServiceProvider implements ServiceProviderInterface
+{
+    public function register (Application $app)
+    {
+
+    }
+
+    public function boot (Application $app)
+    {
+
+    }
+}
